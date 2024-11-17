e300654f2e32f3090156420d3c4aeb9ad2988d49
huynhmanh219 <135082065+huynhmanh219@users.noreply.github.com>
2024-11-17 13:06:36 +0700

    Danh sách tài khoản

--- controller/admin/account.controller.js
+++ controller/admin/account.controller.js
@@ -1,34 +1,62 @@
 const Account = require("../../models/account.model");
 const systemConfig = require("../../config/system");
-
+const Role = require("../../models/role.model");
+const md5 = require("md5");
 
 //[GET] admin/accounts
 module.exports.index = async (req,res) =>{
     const find = {
         deleted:false
     }
-    const records = await Account.find(find);
+    const records = await Account.find(find).select("-password -token"); // loại trừ các trường như password, token để bảo mật dữ liệu
+    
+    for (const record of records) {
+        const role = await Role.findOne({
+            _id: record.role_id,
+            deleted:false
+    })
+    record.role = role;
+}
+    
     res.render(`admin/pages/accounts/index`,{
         pageTitle:"Danh Sách Tài Khoản",
-        records:records
+        accounts:records
     });
 }
 
 //[GET] admin/accounts/create
 module.exports.create = async (req,res) =>{
 
+    
+    const roles = await Role.find({deleted:false});
     res.render(`admin/pages/accounts/create`,{
-        pageTitle:"Danh Sách Tài Khoản"
+        pageTitle:"Danh Sách Tài Khoản",
+        roles:roles
     });
 
 }
 
 //[POST] admin/accounts/create
 module.exports.createPost = async (req,res) =>{
-    
-    console.log(req.body.fullname);
-    const record = new Account(req.body);
-    await record.save();
+    const emailExist = await Account.findOne({
+        deleted:false,
+        email:req.body.email
+    })
+    if(emailExist)
+    {
+        req.flash("error",`Email ${req.body.email} đã tồn tại !!!!`);
+        res.redirect("back");
+    }
+    else{
+        req.body.password = md5(req.body.password); 
+        const record = new Account(req.body);
+        await record.save();
+        res.redirect(`${systemConfig.prefixAdmin}/accounts`);
+    }
 
-    res.redirect(`${systemConfig.prefixAdmin}/accounts`);
+}
+
+//[GET] admin/account/edit/id
+module.exports.edit = async(req,res)=>{
+    
 }
--- routes/admin/account.route.js
+++ routes/admin/account.route.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+const upload = multer();
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware");
+const validate = require("../../validates/admin/account.validate");
 
 const controller = require("../../controller/admin/account.controller");
 
 router.get("/",controller.index);
 router.get("/create",controller.create);
-router.post("/create",controller.createPost);
+
+router.post("/create",
+    upload.single("avatar"),
+    uploadCloud.upload,
+    validate.creatPost,
+    controller.createPost);
+
+router.get("/edit/:id",controller.edit);
 
 module.exports = router;
