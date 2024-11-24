b996af0c5b57791dbb0e7cbdf6fc4f9198457f29
huynhmanh219 <135082065+huynhmanh219@users.noreply.github.com>
2024-11-24 13:55:14 +0700

    Trang danh sách sản phẩm bên client

--- controller/client/product.controller.js
+++ controller/client/product.controller.js
@@ -1,7 +1,8 @@
 const productHelper = require("../../helper/product");
 const ProductCategory = require("../../models/product-category.model");
 const Product = require("../../models/product.model");
-
+const { all } = require("../../routes/client/product.route");
+const ProductCategoryHelper = require("../../helper/productCategory");
 
 //[get] product
 module.exports.index = async(req,res)=>{
@@ -20,17 +21,32 @@ module.exports.index = async(req,res)=>{
 }
 //[get] product/slugCategory
 module.exports.category = async(req,res)=>{
-    const Category = await ProductCategory.findOne({
+    const category = await ProductCategory.findOne({
         slug:req.params.slugCategory,
-        deleted:false
+        deleted:false,
+        status:"active"
     })
-    
-    res.send("ok");
+
+    const listSubCategory = await ProductCategoryHelper.getSubCategory(category.id)
+    const listSubCategoryId = listSubCategory.map(item => item.id);
+   
+    const products = await Product.find({
+        product_category_id: {$in: [category._id,...listSubCategoryId]},
+        deleted:false
+    }).sort({position:"desc"})
+
+    const newproduct = productHelper.priceNewproduct(products);
+   
+    res.render("client/pages/products/index",{
+        pageTitle:category.title,
+        products:newproduct 
+    });
 }
 
 module.exports.create = (req,res)=>{
     res.render("client/pages/products/index")
 }
+
 module.exports.edit = (req,res)=>{
     res.render("client/pages/products/index")
 }
---
+++ helper/productCategory.js
@@ -0,0 +1,19 @@
+const ProductCategory = require("../models/product-category.model");
+module.exports.getSubCategory = async(parentId) =>{
+    const getCategory = async (parentId) =>
+        {
+            const subs = await ProductCategory.find({
+                parent_id:parentId,
+                status:"active",
+                deleted:false
+            })
+            let allsubs = [...subs];
+            for (const sub of subs) {
+                const childs = await getCategory(sub.id);
+                allsubs = allsubs.concat(childs);
+            }
+            return allsubs;
+        };
+    const result = getCategory(parentId);
+    return result;
+}
