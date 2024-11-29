d02af2a3f5aa573554ca80580c4fed3ba6fa3695
huynhmanh219 <135082065+huynhmanh219@users.noreply.github.com>
2024-11-29 21:03:50 +0700

    CartMini header

--- middlewares/client/cart.middleware.js
+++ middlewares/client/cart.middleware.js
@@ -9,14 +9,18 @@ module.exports.cartId = async (req,res,next)=>{
         console.log(cart);
         // có thể lưu ip của khách hàng bằng javascript
 
-        
+
         res.cookie("cartId",cart.id,{
             expires: new Date(Date.now() + expiresCookies)
         });
     }
     else
     {
-
+        const cart = await Cart.findOne({
+            _id:req.cookies.cartId
+        })
+        cart.totalQuantity = cart.products.reduce((sum,item)=> sum+item.quantity,0);       
+        res.locals.miniCart= cart;
     }
 
     next();
