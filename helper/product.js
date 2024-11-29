8df9167c2ebb183da5c5a357add436fa38979b08
huynhmanh219 <135082065+huynhmanh219@users.noreply.github.com>
2024-11-29 22:07:37 +0700

    Làm Trang giỏ hàng

--- helper/product.js
+++ helper/product.js
@@ -8,5 +8,5 @@ module.exports.priceNewproducts = (products)=>{
 
 module.exports.priceNewproduct = (product)=>{
     const priceNew =  (product.price -(100 *product.discountPercentage)/100).toFixed(0);
-    return priceNew;
+    return parseInt(priceNew);
 }
